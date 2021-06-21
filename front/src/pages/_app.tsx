2ade9eeb6efc0c363d7fea8326efec9324095026
HongJunHyeok <edb1631@naver.com>
2021-06-21 15:37:57 +0900

    SET: next-redux-wrapper & saga

--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from "next/app";
 import Head from "next/head";
+import wrapper from "store/configureStore";
 import GlobalStyle from "styles/GlobalStyle";
 
 const MyApp = ({ Component }: AppProps) => {
@@ -9,13 +10,13 @@ const MyApp = ({ Component }: AppProps) => {
         <meta charSet="utf-8" />
         <meta name="description" content="개발을 공유하다." />
         <title>HLOG</title>
+        <GlobalStyle />
       </Head>
       <body>
-        <GlobalStyle />
         <Component />
       </body>
     </>
   );
 };
 
-export default MyApp;
+export default wrapper.withRedux(MyApp);
