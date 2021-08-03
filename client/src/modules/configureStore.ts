51d83967588cd1b5009a8fdb1740a5f36fa7b0fc
HongJunHyeok <edb1631@naver.com>
2021-08-04 06:56:00 +0900

    ADD: Create configure store & wrapper

---
+++ client/src/modules/configureStore.ts
@@ -0,0 +1,21 @@
+import { createWrapper } from "next-redux-wrapper";
+import { applyMiddleware, compose, createStore } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { isDevMode } from "../utils/isDevMode";
+
+const configureStore = () => {
+  const middlewares = [];
+
+  const enhancer = isDevMode()
+    ? composeWithDevTools(applyMiddleware(...middlewares))
+    : compose(applyMiddleware(...middlewares));
+
+  const store = createStore(rootReducer, enhancer);
+  return store;
+};
+
+const wrapper = createWrapper(configureStore, {
+  debug: isDevMode() ? true : false,
+});
+
+export default wrapper;
